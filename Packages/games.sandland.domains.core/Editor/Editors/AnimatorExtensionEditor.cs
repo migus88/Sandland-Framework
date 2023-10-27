bf20596a75d2c438b1b070276438f4d24526dc1e
Yuri Sokolov <7556847+migus88@users.noreply.github.com>
2023-10-27 21:32:03 +0300

    animator extension editor

---
+++ Packages/games.sandland.domains.core/Editor/Editors/AnimatorExtensionEditor.cs
@@ -0,0 +1,113 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Animations;
+using System.IO;
+using System.Text;
+using Sandland.Core.Runtime.Utils;
+using UnityEditor.PackageManager;
+
+namespace Sandland.Domains.Core.Inspectors
+{
+    [CustomEditor(typeof(Animator))]
+    public class AnimatorExtensionEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            var animator = (Animator)target;
+
+            if (!GUILayout.Button("Generate C# Class"))
+            {
+                return;
+            }
+
+            var path = EditorUtility.OpenFolderPanel("Save C# Class", string.Empty, string.Empty);
+
+            if (string.IsNullOrEmpty(path))
+            {
+                return;
+            }
+            
+            path = Path.Combine(path, $"{animator.runtimeAnimatorController.name.ToPascalCase()}Animations.cs");
+            GenerateClass(animator, path);
+        }
+
+        private void GenerateClass(Animator animator, string path)
+        {
+            var controller = animator.runtimeAnimatorController as AnimatorController;
+
+            if (controller == null)
+            {
+                Debug.LogError("Runtime controller is not found");
+                return;
+            }
+
+            var animations = new HashSet<string>();
+
+            foreach (var layer in controller.layers)
+            {
+                TraverseStateMachine(layer.stateMachine, animations);
+            }
+
+            var content = GetClassContent(animations, controller.name.ToPascalCase());
+            File.WriteAllText(path, content);
+            
+            AssetDatabase.Refresh();
+            Client.Resolve();
+        }
+
+        private string GetClassContent(HashSet<string> animations, string controllerName)
+        {
+
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine("using UnityEngine;");
+            stringBuilder.AppendLine();
+            stringBuilder.AppendLine($"public static class {controllerName}Animations");
+            stringBuilder.AppendLine("{");
+
+            foreach (var animation in animations)
+            {
+                stringBuilder.AppendLine($"\tpublic static readonly int {animation.ToPascalCase()} = Animator.StringToHash(\"{animation}\");");
+            }
+
+            stringBuilder.AppendLine("}");
+
+            return stringBuilder.ToString();
+        }
+
+        private void TraverseStateMachine(AnimatorStateMachine stateMachine, HashSet<string> animations)
+        {
+            foreach (var state in stateMachine.states)
+            {
+                HandleAnimatorState(animations, state.state.motion, state.state.name);
+            }
+
+            foreach (var subStateMachine in stateMachine.stateMachines)
+            {
+                TraverseStateMachine(subStateMachine.stateMachine, animations);
+            }
+        }
+
+        private void TraverseBlendTree(BlendTree blendTree, HashSet<string> animations)
+        {
+            foreach (var childMotion in blendTree.children)
+            {
+                HandleAnimatorState(animations, childMotion.motion, childMotion.motion.name);
+            }
+        }
+
+        private void HandleAnimatorState(HashSet<string> animations, Motion motion, string animationName)
+        {
+            if (motion is BlendTree tree)
+            {
+                TraverseBlendTree(tree, animations);
+                return;
+            }
+
+            animations.Add(animationName);
+        }
+    }
+}
