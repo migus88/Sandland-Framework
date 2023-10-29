b5d30c2dd96f20c8843cc1bc80489426ef70ff1b
Yuri Sokolov <7556847+migus88@users.noreply.github.com>
2023-10-29 11:12:06 +0200

    scene management package created

---
+++ Packages/games.sandland.scene-management/Tests/CurrentPackageInfo.cs
@@ -0,0 +1,7 @@
+namespace Sandland.SceneManagement.Tests
+{
+    internal static class CurrentPackageInfo
+    {
+        public static readonly string PackageName = "games.sandland.scene-management";
+    }
+}
